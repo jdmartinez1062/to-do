ab7f0bdc2539f93db04586c749f2cf6be3093110
wrakc <carlosveiga1618@gmail.com>
2021-01-11 17:31:39 -0300

    add New Project button in the navbar to always be visible

--- src/DOM.js
+++ src/DOM.js
@@ -2,22 +2,26 @@ import { tabCreation, tabUpdate } from "./tab";
 import form from "./form";
 import { showProject } from "./showProject";
 const init = () => {
+  const navbar = document.getElementById("navbar")
   const content = document.getElementById("content");
+  const body = document.getElementById("main")
   const sidebar = document.createElement("div");
   const mainDiv = document.createElement("div");
   const formButton = document.createElement("input");
 
+  body.append(navbar);
+  navbar.append(formButton);
+  navbar.classList.add('navbar', 'is-fixed-top');
   formButton.id = "form-button";
   formButton.type = "submit";
   formButton.value = 'New Project'
-  formButton.classList.add('button', 'is-primary', 'column');
-
+  formButton.classList.add('button', 'is-primary', 'is-fullwidth');
   formButton.addEventListener("click", form);
-
+ 
   sidebar.setAttribute("id", "sidebar");
   mainDiv.setAttribute("id", "main-div");
-  content.appendChild(sidebar);
-  content.append(formButton, mainDiv);
+  content.append(sidebar);
+  content.append(mainDiv);
   sidebar.appendChild(tabCreation());
   console.log(localStorage.getItem("Projects"));
   const defaultProject = JSON.parse(localStorage.getItem("Projects"));
